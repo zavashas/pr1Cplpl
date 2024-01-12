2fb882c9c541117ac85315275939c54de4ba91c7
zavashas <144954320+zavashas@users.noreply.github.com>
2024-01-12 13:08:01 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    std::cout << "У лукоморья дуб зелёный,\nЗлатая цепь на дубе том:\nИ днём и ночью кот \"учёный\"\nВсё ходит по цепи кругом";
+    
+}
